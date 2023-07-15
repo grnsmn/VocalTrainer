37d4d0f186876b5f86b451192ae7e4323bcecf39
GScriptS <simoneguarnuccio@gmail.com>
2023-07-15 20:51:22 +0200

    Update VocalizziScreen.js

--- src/screen/VocalizziScreen.js
+++ src/screen/VocalizziScreen.js
@@ -1,34 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, View, Button, FlatList, Text } from "react-native";
-import firebase from "firebase/compat/app";
 import { getStorage, ref, getDownloadURL, list } from "firebase/storage";
 import { Audio } from "expo-av";
 
-
-// Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyAY9RAEFc8VYup5Y8SQZziZS-GPDAodBE0",
-  authDomain: "vocaltrainer-bfc85.firebaseapp.com",
-  databaseURL:
-    "https://vocaltrainer-bfc85-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "vocaltrainer-bfc85",
-  storageBucket: "gs://vocaltrainer-bfc85.appspot.com",
-  messagingSenderId: "976841336358",
-  appId: "1:976841336358:web:c3fd687505c9314e0368e7",
-  measurementId: "G-4BV0XVCFBG",
-};
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-} else {
-  firebase.app(); // if already initialized, use that one
-}
-
-const storage = getStorage();
-
 const storageRef = ref(
   storage,
-  "gs://vocaltrainer-bfc85.appspot.com"
+  "gs://vocaltrainer-bfc85.appspot.com/81 Traccia 81.mp3"
 );
 
 const VocalizziScreen = ({ route, navigation }) => {
