dce04195f743be784580102175b475a5b9de1045
GScriptS <simoneguarnuccio@gmail.com>
2023-07-22 12:22:05 +0200

    feat: start to render all exercice in storage cloud

--- src/screen/VocalizziScreen.js
+++ src/screen/VocalizziScreen.js
@@ -1,71 +1,94 @@
-import React, { useState, useEffect } from "react";
-import { StyleSheet, View, Button, FlatList, Text } from "react-native";
-import firebase from "firebase/compat/app";
-import { getStorage, ref, getDownloadURL, list } from "firebase/storage";
-import { Audio } from "expo-av";
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Button, Text } from 'react-native';
+import { getStorage, ref, getDownloadURL, list } from 'firebase/storage';
+import { Audio } from 'expo-av';
 
 const storage = getStorage();
 
-const storageRef = ref(
-  storage,
-  "gs://vocaltrainer-bfc85.appspot.com/81 Traccia 81.mp3"
-);
+const storageRef = ref(storage, 'gs://vocaltrainer-bfc85.appspot.com');
 
 const VocalizziScreen = ({ route, navigation }) => {
-  const [sound, setSound] = useState('');
-  const [listVocalizzi, setListVocalizzi] = useState();
-  
-  useEffect(() => {
-    const setAudio = async () => {
-      const listStorage = await list(storageRef)
-      setListVocalizzi(listStorage.items);
-      const url = await getDownloadURL(storageRef);
-      console.log("Loading Sound");
-      const { sound: soundFirebase } = await Audio.Sound.createAsync(
-        { uri: url },
-      );
-      setSound(soundFirebase);
-    }
-    setAudio()
-  }, [])
-  
-  async function playSound() {
-    const getSound = await sound.getStatusAsync();
-    if (!getSound.isPlaying) {
-      console.log("Playing Sound");
-      await sound.playAsync();
-    } else {
-      console.log("Pause Sound");
-      await sound.stopAsync();
-    }
-  }
+	const [sound, setSound] = useState('');
+	const [soundChoose, setSoundChoose] = useState('');
+	const [listVocalizzi, setListVocalizzi] = useState();
 
-  useEffect(() => {
-    return sound
-      ? () => {
-          console.log("Unloading Sound");
-          sound.unloadAsync();
-        }
-      : undefined;
-  }, [sound]);
+	useEffect(() => {
+		const setAudio = async () => {
+			const listStorage = await list(storageRef);
 
-  return (
-    <View style={styles.container}>
-      {listVocalizzi?.map(item => {
-        return (<View style={{width:10, height:10, backgroundColor:'red', padding: 8}}>{ item._location.path}</View>)
-      	})}
-      <Button title="Play Sound" onPress={playSound} />
-    </View>
-  );
+			setListVocalizzi(listStorage?.items);
+
+			if (soundChoose !== '') {
+				const soundRef = ref(
+					storage,
+					`gs://vocaltrainer-bfc85.appspot.com/${soundChoose}`,
+				);
+				const uri = await getDownloadURL(soundRef);
+				console.log('Loading Sound');
+				const { sound: soundFirebase } = await Audio.Sound.createAsync({
+					uri,
+				});
+				setSound(soundFirebase);
+			}
+		};
+		setAudio();
+	}, [soundChoose]);
+
+	
+	useEffect(() => {
+		const play = async () => {
+			playSound();
+		};
+		play();
+		return sound
+		? () => {
+			console.log('Unloading Sound');
+			sound.unloadAsync();
+		}
+		: undefined;
+	}, [sound]);
+	
+	async function playSound() {
+		if (sound) {
+			const getSound = await sound?.getStatusAsync();
+			if (!getSound.isPlaying) {
+				console.log('Playing Sound');
+				await sound.playAsync();
+			} else {
+				console.log('Pause Sound');
+				await sound.stopAsync();
+				setSoundChoose('');
+				setSound('');
+			}
+		}
+	}
+	return (
+		<View style={styles.container}>
+			{listVocalizzi?.map(item => {
+				return (
+					<View
+						style={{
+							width: '100%',
+							height: 100,
+							backgroundColor: 'lightgreen',
+						}}
+					>
+						<Button title={item?._location?.path}  onPress={() => setSoundChoose(item?._location?.path)} style={{color: 'black'}} />
+					</View>
+				);
+			})}
+			<Button title="bbhb" onPress={playSound} />
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    // alignItems: 'center',
-    justifyContent: "space-around",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		// alignItems: 'center',
+		justifyContent: 'space-around',
+	},
 });
 
 export default VocalizziScreen;
