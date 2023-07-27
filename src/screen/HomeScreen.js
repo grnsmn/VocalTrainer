fcee2faa9e9fcd4d25892e534e5c57abd429a92a
GScriptS <simoneguarnuccio@gmail.com>
2023-07-27 22:57:33 +0200

    feat: integrated service initFirebase into HomeScreen

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -2,35 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, RefreshControl, Button } from 'react-native';
 import { useEffect, useState } from 'react';
 import { getDatabase, ref, onValue } from 'firebase/database';
-import firebase from 'firebase/compat/app';
-import {
-	API_KEY,
-	AUTH_DOMAIN,
-	DATABASE_URL,
-	PROJECT_ID,
-	STORAGE_BUCKET,
-	MESSAGING_SENDER_ID,
-	APP_ID,
-	MEASUREMENT_ID,
-} from '@env';
+import useFirebaseInit from '../services/initFirebase';
 
-// Initialize Firebase
-const firebaseConfig = {
-	apiKey: API_KEY,
-	authDomain: AUTH_DOMAIN,
-	databaseURL:DATABASE_URL,
-	projectId: PROJECT_ID,
-	storageBucket: STORAGE_BUCKET,
-	messagingSenderId: MESSAGING_SENDER_ID,
-	appId: APP_ID,
-	measurementId: MEASUREMENT_ID,
-};
-
-if (!firebase.apps.length) {
-	firebase.initializeApp(firebaseConfig);
-} else {
-	firebase.app(); // if already initialized, use that one
-}
+useFirebaseInit()
 
 const db = getDatabase();
 const refRespirazione = ref(db, '/Respirazione/');
