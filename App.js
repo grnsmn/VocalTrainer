c662d6fe38dfe0d622f98d86d8159bb29eb461bc
GScriptS <simoneguarnuccio@gmail.com>
2024-01-30 23:29:07 +0100

    feat: start to prepare 2.0 renewed version

--- App.js
+++ App.js
@@ -1,75 +1,20 @@
-import { useEffect } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './src/screen/HomeScreen';
-import EserciziList from './src/Respirazione/Famiglia/EserciziList';
-import FamiglieList from './src/Respirazione/FamiglieList';
-import TrainingScreen from './src/screen/TrainingScreen';
-import VocalizziCategoryList from './src/screen/VocalizziCategoryList';
-import VocalsChoose from './src/screen/VocalsChoose';
-//import Metronome from './src/MetronomeHook'
-import * as Font from 'expo-font';
-import VocalizziList from './src/components/VocalizziList';
-
-const fetchFonts = () => {
-	return Font.loadAsync({
-		'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
-	});
-};
-
-const Stack = createNativeStackNavigator();
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-	useEffect(() => {
-		fetchFonts();
-	}, []);
-
-	return (
-		<NavigationContainer>
-			<Stack.Navigator
-				initialRouteName="Home"
-				screenOptions={navScreensOptions}
-			>
-				<Stack.Screen
-					name="Home"
-					component={HomeScreen}
-					options={{ title: 'Vocal Trainer' }}
-				/>
-				<Stack.Screen
-					name="Lista Famiglie"
-					component={FamiglieList}
-					options={{ title: 'Famiglie' }}
-				/>
-				<Stack.Screen
-					name="Lista esercizi"
-					component={EserciziList}
-					options={({ route }) => ({ title: route.params.name })}
-				/>
-				<Stack.Screen
-					name="Training"
-					component={TrainingScreen}
-					options={({ route }) => ({ title: route.params.name })}
-				/>
-				<Stack.Screen name="VocalsChoose" component={VocalsChoose} />
-				<Stack.Screen name="Vocalizzi" component={VocalizziCategoryList} />
-				<Stack.Screen
-					name="VocalizziList"
-					component={VocalizziList}
-					options={({ route }) => ({ title: route.params.name })}
-				/>
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-const navScreensOptions = {
-	headerTitleAlign: 'center',
-	headerStyle: {
-		backgroundColor: '#1CC625',
-	},
-	headerTitleStyle: {
-		fontWeight: 'bold',
-		fontSize: 30,
-		textAlign: 'center',
-	},
-};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
