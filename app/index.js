f3b392886c8dd4d988b036014c9baf6e3519a3da
GScriptS <simoneguarnuccio@gmail.com>
2024-02-05 19:44:12 +0100

    feat: tmp remove use of gluestack

--- app/index.js
+++ app/index.js
@@ -1,13 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { GluestackUIProvider, Text, Box } from '@gluestack-ui/themed';
-import { config } from '@gluestack-ui/config';
+import { Text } from 'react-native';
 
 export default function Page() {
-	return (
-		<GluestackUIProvider config={config}>
-			<Box width="100%" justifyContent="center" alignItems="center">
-				<Text>Open up App.js to start working on your app!</Text>
-			</Box>
-		</GluestackUIProvider>
-	);
+	return <Text>Home page</Text>;
 }
