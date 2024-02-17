62b0b65c36699c9eefaffecf115639bf982ba920
GScriptS <simoneguarnuccio@gmail.com>
2024-02-17 10:26:24 +0100

    feat: wrap app with navigator

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { GluestackUIProvider, Text, Box } from '@gluestack-ui/themed';
 import { config } from '@gluestack-ui/config';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
 	return (
 		<GluestackUIProvider config={config}>
-			<Box width="100%" justifyContent="center" alignItems="center">
-				<Text>Open up App.js to start working on your app!</Text>
-			</Box>
+			<NavigationContainer>
+				<Box width="100%" justifyContent="center" alignItems="center">
+					<Text>Open up App.js to start working on your app!</Text>
+				</Box>
+			</NavigationContainer>
 		</GluestackUIProvider>
 	);
 }
