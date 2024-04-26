32169745c45844583e5776322800937adb7f6a5b
Simone Guarnuccio <simoneguarnuccio@creationdose.com>
2024-04-26 23:52:09 +0200

    feat: wrap app with nav container

--- App.js
+++ App.js
@@ -6,18 +6,21 @@ import {
 	StatusBar,
 } from '@gluestack-ui/themed';
 import { config } from '@gluestack-ui/config';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
 	return (
-		<GluestackUIProvider config={config}>
-			<SafeAreaView flex={1}>
-				<StatusBar />
-				<Box flex={1} justifyContent="center" alignItems="center">
-					<Text textAlign="center">
-						Open up App.js to start working on your app!
-					</Text>
-				</Box>
-			</SafeAreaView>
-		</GluestackUIProvider>
+		<NavigationContainer>
+			<GluestackUIProvider config={config}>
+				<SafeAreaView flex={1}>
+					<StatusBar />
+					<Box flex={1} justifyContent="center" alignItems="center">
+						<Text textAlign="center">
+							Open up App.js to start working on your app!
+						</Text>
+					</Box>
+				</SafeAreaView>
+			</GluestackUIProvider>
+		</NavigationContainer>
 	);
 }
