d18769947cdaefbbb90c51652084b6db589de2d0
Simone Guarnuccio <simoneguarnuccio@creationdose.com>
2024-04-27 01:44:21 +0200

    feat: continuo to create Home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,45 +1,26 @@
 import React from 'react';
 import {
-	AddIcon,
 	Card,
 	ChevronsRightIcon,
-	Fab,
-	FabIcon,
-	FabLabel,
+	FlatList,
 	HStack,
 	Heading,
 	Icon,
 	Pressable,
 } from '@gluestack-ui/themed';
+import CardSelect from '../../components/CardSelect';
+
+const TYPES_OF_VOCALS = [{ label: 'Trilli' }, { label: 'Humming' }];
 
 export const Home = () => {
 	const handlePress = async () => {
 		console.log('🚀 ~ testing');
 	};
-	return (
-		<>
-			<Pressable onPress={handlePress} bg="primary.400" p="20">
-				<Card size="md" variant="elevated" m="$3">
-					<HStack justifyContent="space-between" alignItems="center">
-						<Heading mb="$1" size="md">
-							Quick Start
-						</Heading>
 
-						<Icon as={ChevronsRightIcon} size="lg" />
-					</HStack>
-				</Card>
-			</Pressable>
+	const renderItem = ({ item }) => {
+		const { label } = item;
+		return <CardSelect title={label} onPress={handlePress} />;
+	};
 
-			<Fab
-				size="md"
-				placement="bottom right"
-				isHovered={false}
-				isDisabled={false}
-				isPressed={false}
-			>
-				<FabIcon as={AddIcon} mr="$1" />
-				<FabLabel>Quick start</FabLabel>
-			</Fab>
-		</>
-	);
+	return <FlatList data={TYPES_OF_VOCALS} renderItem={renderItem} />;
 };
