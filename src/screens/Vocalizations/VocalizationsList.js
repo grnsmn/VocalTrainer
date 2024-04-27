7e4d420fa45ca67dbf82e8e2ffd908bc982bc91c
Simone Guarnuccio <simoneguarnuccio@creationdose.com>
2024-04-27 23:50:04 +0200

    feat: start to request storage from firebase in vocalizationsList

--- src/screens/Vocalizations/VocalizationsList.js
+++ src/screens/Vocalizations/VocalizationsList.js
@@ -1,8 +1,13 @@
 import { Text, Box } from '@gluestack-ui/themed';
 
 import React from 'react';
+import { getStorage, ref, getDownloadURL, list } from 'firebase/storage';
+// import { Audio } from 'expo-av';
+// import { STORAGE_PATH } from '@env';
 
 const VocalizationsList = () => {
+	const storage = getStorage();
+
 	return (
 		<Box flex={1} justifyContent="center" alignItems="center">
 			<Text color="white" fontWeight="$bold">
