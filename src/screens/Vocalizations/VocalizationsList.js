a4b463223e22e32732f3e863efc20ca955796d0f
Simone Guarnuccio <simoneguarnuccio@creationdose.com>
2024-04-28 00:17:56 +0200

    feat: start to require exercices from firebase storage

--- src/screens/Vocalizations/VocalizationsList.js
+++ src/screens/Vocalizations/VocalizationsList.js
@@ -1,19 +1,57 @@
-import { Text, Box } from '@gluestack-ui/themed';
+import { FlatList, Text } from '@gluestack-ui/themed';
 
-import React from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
+import { STORAGE_PATH } from '@env';
 import { getStorage, ref, getDownloadURL, list } from 'firebase/storage';
+import CardSelect from '../../components/CardSelect';
 // import { Audio } from 'expo-av';
-// import { STORAGE_PATH } from '@env';
 
-const VocalizationsList = () => {
+const VocalizationsList = ({ route }) => {
+	const { typeVocal, selectedListName } = route.params;
+	const [listVocalizzi, setListVocalizzi] = useState();
+
+	/* ----------------------------- STORAGE REQUIRE ---------------------------- */
 	const storage = getStorage();
+	const endpoint = `${STORAGE_PATH}/${typeVocal}/${selectedListName}`;
+	const storageRef = ref(storage, endpoint);
+
+	useEffect(() => {
+		const setList = async () => {
+			const listStorage = await list(storageRef);
+
+			setListVocalizzi(listStorage?.items);
+		};
+		setList();
+	}, []);
+
+	const renderItem = ({ item }) => {
+		const regex = /\btraccia\s(?:[1-9]|[1-9]\d|100)\b/i;
+		const match = item?._location?.path.match(regex);
+
+		if (match) {
+			const title = match[0]; // Estrae la sottostringa corrispondente al pattern
+			return (
+				<CardSelect
+					title={title}
+					onPress={() => console.log('🚀 ~ testing')}
+				/>
+			);
+		} else {
+			return null;
+		}
+	};
 
 	return (
-		<Box flex={1} justifyContent="center" alignItems="center">
-			<Text color="white" fontWeight="$bold">
-				BOX
-			</Text>
-		</Box>
+		<Suspense fallback={<Text>loading...</Text>}>
+			<FlatList
+				data={listVocalizzi}
+				renderItem={renderItem}
+				// contentContainerStyle={styles.container}
+				// ListFooterComponent={
+
+				// }
+			/>
+		</Suspense>
 	);
 };
 export default VocalizationsList;
