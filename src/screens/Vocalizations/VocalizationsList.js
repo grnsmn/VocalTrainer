e6f6f0942255480d32ee4c8573b07f854eeda0fd
Simone Guarnuccio <simoneguarnuccio@creationdose.com>
2024-04-27 20:00:29 +0200

    feat: created vocalizations list screen

---
+++ src/screens/Vocalizations/VocalizationsList.js
@@ -0,0 +1,14 @@
+import { Text, Box } from '@gluestack-ui/themed';
+
+import React from 'react';
+
+const VocalizationsList = () => {
+	return (
+		<Box flex={1} justifyContent="center" alignItems="center">
+			<Text color="white" fontWeight="$bold">
+				BOX
+			</Text>
+		</Box>
+	);
+};
+export default VocalizationsList;
