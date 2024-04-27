0776778e922597f1401a36811e7e409c6992ae29
Simone Guarnuccio <simoneguarnuccio@creationdose.com>
2024-04-27 21:32:18 +0200

    refactor(App): general refactor

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { GluestackUIProvider, StatusBar } from '@gluestack-ui/themed';
 import { config } from '@gluestack-ui/config';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Home } from './src/screens/Home';
+import { Vocalizations } from './src/screens/Vocalizations';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AudioLines } from 'lucide-react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import VocalizationsList from './src/screens/Vocalizations/VocalizationsList';
 
 const Tab = createBottomTabNavigator();
+const VocalizationsStack = createNativeStackNavigator();
+
+function VocalizationsStackScreen() {
+	return (
+		<VocalizationsStack.Navigator
+			screenOptions={{
+				headerTitleAlign: 'center',
+				headerStyle: { backgroundColor: '#c6e9ff' },
+			}}
+		>
+			<VocalizationsStack.Screen name="Home" component={Vocalizations} />
+			<VocalizationsStack.Screen
+				name="Lista"
+				component={VocalizationsList}
+			/>
+		</VocalizationsStack.Navigator>
+	);
+}
 
 export default function App() {
 	return (
@@ -16,17 +36,19 @@ export default function App() {
 					<StatusBar />
 					<Tab.Navigator
 						screenOptions={({ route }) => ({
-							tabBarIcon: ({ focused, color, size }) => {
+							tabBarIcon: ({ focused }) => {
 								if (route.name === 'Vocalizzi') {
 									return <AudioLines />;
 								}
 							},
 							tabBarInactiveTintColor: 'gray',
-							headerTitleAlign: 'center',
+							headerShown: false,
 						})}
 					>
-						<Tab.Screen name="Vocalizzi" component={Home} />
-						{/* <Tab.Screen name="Respirazione" component={Home} /> */}
+						<Tab.Screen
+							name="Vocalizzi"
+							component={VocalizationsStackScreen}
+						/>
 					</Tab.Navigator>
 				</GluestackUIProvider>
 			</SafeAreaProvider>
