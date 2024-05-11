37b5f4201f23989dd126298bf6a5a7be233a679b
Simone Guarnuccio <simoneguarnuccio@creationdose.com>
2024-05-11 22:50:52 +0200

    size: customize size right icon card select

--- src/components/CardSelect.js
+++ src/components/CardSelect.js
@@ -19,7 +19,7 @@ const CardSelect = ({ onPress, title }) => {
 				<HStack justifyContent="space-between" alignItems="center">
 					<Heading size="md">{title}</Heading>
 
-					<Icon as={ChevronsRightIcon} size="lg" />
+					<Icon as={ChevronsRightIcon} size="xl" />
 				</HStack>
 			</Card>
 		</Pressable>
