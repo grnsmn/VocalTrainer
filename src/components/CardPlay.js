f4d8a6d4cfcd05037824fd8ecb8335d38d5b3fcb
Simone Guarnuccio <simoneguarnuccio@creationdose.com>
2024-05-26 15:36:12 +0200

    feat(CardPlay): Update with animated icon

--- src/components/CardPlay.js
+++ src/components/CardPlay.js
@@ -7,6 +7,8 @@ import {
 	Pressable,
 	Spinner,
 } from '@gluestack-ui/themed';
+import LottieView from 'lottie-react-native';
+import AnimatedPlayIcon from '../../assets/lotties/sing.json';
 
 const CardPLay = ({
 	onPress,
@@ -15,6 +17,7 @@ const CardPLay = ({
 	isPlaying = false,
 	isLoading = false,
 }) => {
+	const showRightIcon = RightIcon && !isPlaying && !isLoading;
 	const handleOnPress = () => {
 		onPress(title);
 	};
@@ -28,10 +31,17 @@ const CardPLay = ({
 				<HStack justifyContent="space-between" alignItems="center">
 					<Heading size="md">{title}</Heading>
 					{isLoading && <Spinner size="small" color={'$green700'} />}
-					{RightIcon && (
+					{showRightIcon && (
 						<Icon
 							as={RightIcon}
-							color={isPlaying ? '$green700' : undefined}
+							// color={isPlaying ? '$green700' : undefined}
+						/>
+					)}
+					{!showRightIcon && (
+						<LottieView
+							source={AnimatedPlayIcon}
+							autoPlay
+							style={{ width: 28, height: 28 }}
 						/>
 					)}
 				</HStack>
