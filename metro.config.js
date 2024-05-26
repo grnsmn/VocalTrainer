9c9297a6c552016a675acf67f83e77094198f173
Simone Guarnuccio <simoneguarnuccio@creationdose.com>
2024-05-26 16:26:55 +0200

    feat: test to integrate rn support for web lottie

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,17 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname, {
-	// Additional features...
-});
+const config = getDefaultConfig(__dirname);
+
+config.resolver = {
+	...config.resolver,
+	extraNodeModules: {
+		'lottie-react-native': path.resolve(
+			__dirname,
+			'node_modules/react-native-web-lottie',
+		),
+	},
+};
 
 module.exports = config;
