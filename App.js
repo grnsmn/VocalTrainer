0da1dc779a56d04daae95e5267e5758818b3292f
Simone Guarnuccio <simoneguarnuccio@creationdose.com>
2024-05-26 15:54:53 +0200

    feat(App): Add Breathing tab section

--- App.js
+++ App.js
@@ -3,14 +3,16 @@ import { config } from '@gluestack-ui/config';
 import { NavigationContainer } from '@react-navigation/native';
 import { Vocalizations } from './src/screens/Vocalizations';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { AudioLines } from 'lucide-react-native';
+import { AudioLines, Wind } from 'lucide-react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import VocalizationsList from './src/screens/Vocalizations/VocalizationsList';
 import useFirebaseInit from './src/hooks/useFirebaseInit';
+import VocalizationsList from './src/screens/Vocalizations/VocalizationsList';
+import CategoriesBreath from './src/screens/Breathing/CategoriesBreath';
 
 const Tab = createBottomTabNavigator();
 const VocalizationsStack = createNativeStackNavigator();
+const BreathingStack = createNativeStackNavigator();
 
 function VocalizationsStackScreen() {
 	const getDynamicHeader = ({ route }) => {
@@ -30,6 +32,7 @@ function VocalizationsStackScreen() {
 			screenOptions={{
 				headerTitleAlign: 'center',
 				headerStyle: { backgroundColor: '#c6e9ff' },
+				title: 'Vocal Trainer',
 			}}
 		>
 			<VocalizationsStack.Screen name="Home" component={Vocalizations} />
@@ -39,6 +42,35 @@ function VocalizationsStackScreen() {
 				options={getDynamicHeader}
 			/>
 		</VocalizationsStack.Navigator>
+	);
+}
+
+function BreathingStackScreen() {
+	const getDynamicHeader = ({ route }) => {
+		//Replace usato per il caso di route name lunghi passati in
+		//camel case che vengono divisi con spazio per una migliore leggibilità nell'header della UI
+		const dynamicHeaderPart = route.params?.selectedListName
+			.replace(/([A-Z])/g, ' $1')
+			.trim();
+
+		return {
+			title: `Esercizi ${dynamicHeaderPart}` || 'Lista Esercizi',
+		};
+	};
+
+	return (
+		<BreathingStack.Navigator
+			screenOptions={{
+				headerTitleAlign: 'center',
+				headerStyle: { backgroundColor: '#c6e9ff' },
+				title: 'Vocal Trainer',
+			}}
+		>
+			<BreathingStack.Screen
+				name="BreathingFamilies"
+				component={CategoriesBreath}
+			/>
+		</BreathingStack.Navigator>
 	);
 }
 
@@ -60,6 +92,11 @@ export default function App() {
 										/>
 									);
 								}
+								if (route.name === 'Respirazione') {
+									return (
+										<Icon as={Wind} color={'$primary500'} />
+									);
+								}
 							},
 							tabBarInactiveTintColor: 'gray',
 							headerShown: false,
@@ -68,6 +105,10 @@ export default function App() {
 						<Tab.Screen
 							name="Vocalizzi"
 							component={VocalizationsStackScreen}
+						/>
+						<Tab.Screen
+							name="Respirazione"
+							component={BreathingStackScreen}
 						/>
 					</Tab.Navigator>
 				</GluestackUIProvider>
