7b2c1acf216782606325e41d5b1b18c8b34bbf52
Simone Guarnuccio <simoneguarnuccio@creationdose.com>
2024-12-01 23:04:20 +0100

    feat: add redux-devtools-extension for enhanced state management and debugging in zustand store

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,21 @@
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 
-const useStore = create(set => ({
-	auth: {
-		user: null,
-		accessToken: '',
-	},
-	setAuth: newAuth => set(() => ({ auth: newAuth })),
-	setAccessToken: newAccessToken =>
-		set(state => ({
-			auth: {
-				...state.auth,
-				accessToken: newAccessToken,
-			},
-		})),
-}));
+const useStore = create(
+	devtools(
+		set => ({
+			auth: undefined,
+			setAuth: newAuth => set(() => ({ auth: newAuth })),
+			setAccessToken: newAccessToken =>
+				set(state => ({
+					auth: {
+						...state.auth,
+						accessToken: newAccessToken,
+					},
+				})),
+		}),
+		{ name: 'AuthStore' },
+	),
+); // Nome per identificare lo store nei DevTools
 
 export default useStore;
