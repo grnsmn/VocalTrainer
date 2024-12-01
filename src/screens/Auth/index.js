b7da712c9468da746d1505689817e35a8191deb5
GScriptS <simoneguarnuccio@gmail.com>
2024-12-01 21:10:38 +0100

    feat: wrap AuthScreen content in ScrollView for better layout management

--- src/screens/Auth/index.js
+++ src/screens/Auth/index.js
@@ -15,12 +15,15 @@ import {
 	Input,
 	InputField,
 	VStack,
+	ScrollView,
 } from '@gluestack-ui/themed';
 import Hero from '../../components/Hero';
+import useStore from '../../store';
 
 export default function AuthScreen() {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+	const { auth: _auth, setAccessToken } = useStore();
 
 	const auth = getAuth();
 	const provider = new GoogleAuthProvider();
@@ -30,6 +33,7 @@ export default function AuthScreen() {
 	};
 
 	const handleLogin = () => {
+		setAccessToken('ciao');
 		signInWithEmailAndPassword(auth, email, password)
 			.then(userCredential => {
 				alert('Logged in!', `Welcome ${userCredential.user.email}`);
@@ -52,33 +56,35 @@ export default function AuthScreen() {
 	};
 
 	return (
-		<VStack space="sm" alignItems="center" justifyContent="center">
-			<Hero />
-			<Input borderWidth={1} borderRadius="lg">
-				<InputField
-					placeholder="Email"
-					onChangeText={setEmail}
-					bgColor="$blueGray200"
-				/>
-			</Input>
-			<Input borderWidth={1} borderRadius="lg">
-				<InputField
-					placeholder="Password"
-					onChangeText={setPassword}
-					bgColor="$blueGray200"
-				/>
-			</Input>
-			<Button title="Login" onPress={handleLogin}>
-				<ButtonText>Login</ButtonText>
-			</Button>
-			<Button onPress={handleSignup}>
-				<ButtonText>Sign Up</ButtonText>
-			</Button>
-			{Platform.OS === 'web' && (
-				<Button onPress={onPressGoogle}>
-					<ButtonText>Login with Google</ButtonText>
+		<ScrollView>
+			<VStack space="sm" alignItems="center" justifyContent="center">
+				<Hero />
+				<Input borderWidth={1} borderRadius="lg">
+					<InputField
+						placeholder="Email"
+						onChangeText={setEmail}
+						bgColor="$blueGray200"
+					/>
+				</Input>
+				<Input borderWidth={1} borderRadius="lg">
+					<InputField
+						placeholder="Password"
+						onChangeText={setPassword}
+						bgColor="$blueGray200"
+					/>
+				</Input>
+				<Button title="Login" onPress={handleLogin}>
+					<ButtonText>Login</ButtonText>
 				</Button>
-			)}
-		</VStack>
+				<Button onPress={handleSignup}>
+					<ButtonText>Sign Up</ButtonText>
+				</Button>
+				{Platform.OS === 'web' && (
+					<Button onPress={onPressGoogle}>
+						<ButtonText>Login with Google</ButtonText>
+					</Button>
+				)}
+			</VStack>
+		</ScrollView>
 	);
 }
