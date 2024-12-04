cad761d9c0c00139dcf1f100a5a0b0544afd2f7f
Simone Guarnuccio <simoneguarnuccio@creationdose.com>
2024-12-04 22:31:43 +0100

    feat: enhance AuthScreen layout with password visibility toggle and improved input fields

--- src/screens/Auth/index.js
+++ src/screens/Auth/index.js
@@ -17,11 +17,17 @@ import {
 	InputField,
 	VStack,
 	ScrollView,
+	FormControl,
+	Text,
+	InputSlot,
+	InputIcon,
 } from '@gluestack-ui/themed';
 import Hero from '../../components/Hero';
 import useStore from '../../store';
+import { Eye, EyeOff } from 'lucide-react-native';
 
 export default function AuthScreen() {
+	const [showPassword, setShowPassword] = useState(false);
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const { auth: _auth, setAuth } = useStore();
@@ -59,37 +65,84 @@ export default function AuthScreen() {
 		}
 	};
 
+	const handleState = () => {
+		setShowPassword(showState => {
+			return !showState;
+		});
+	};
+
 	return (
-		<ScrollView p={'$4'} bg="$primary0">
-			<VStack space="sm" alignItems="center" justifyContent="center">
+		<ScrollView bg="$primary0">
+			<FormControl
+				p="$4"
+				alignSelf="center"
+				borderRadius="$lg"
+				$dark-borderWidth="$1"
+				$dark-borderRadius="$lg"
+				$dark-borderColor="$borderDark800"
+				maxWidth={350}
+			>
 				<Hero />
-				<Input borderWidth={1} borderRadius="lg">
-					<InputField
-						placeholder="Email"
-						onChangeText={setEmail}
-						bgColor="$blueGray200"
-					/>
-				</Input>
-				<Input borderWidth={1} borderRadius="lg">
-					<InputField
-						placeholder="Password"
-						onChangeText={setPassword}
-						bgColor="$blueGray200"
-						secureTextEntry
-					/>
-				</Input>
-				<Button title="Login" onPress={handleLogin}>
-					<ButtonText>Login</ButtonText>
-				</Button>
-				<Button onPress={handleSignup}>
-					<ButtonText>Sign Up</ButtonText>
-				</Button>
-				{Platform.OS === 'web' && (
-					<Button onPress={onPressGoogle}>
-						<ButtonText>Login with Google</ButtonText>
-					</Button>
-				)}
-			</VStack>
+				<VStack space="xl">
+					<VStack space="xs">
+						<Text color="$text500" lineHeight="$xs">
+							Email
+						</Text>
+						<Input bgColor="$blueGray200" borderRadius="$2xl">
+							<InputField type="text" onChangeText={setEmail} />
+						</Input>
+					</VStack>
+					<VStack space="xs">
+						<Text color="$text500" lineHeight="$xs">
+							Password
+						</Text>
+						<Input
+							textAlign="center"
+							borderWidth={1}
+							bgColor="$blueGray200"
+							borderRadius="$2xl"
+						>
+							<InputField
+								type={showPassword ? 'text' : 'password'}
+								onChangeText={setPassword}
+							/>
+							<InputSlot pr="$3" onPress={handleState}>
+								<InputIcon
+									as={showPassword ? Eye : EyeOff}
+									color="$darkBlue500"
+								/>
+							</InputSlot>
+						</Input>
+					</VStack>
+					<VStack space="lg">
+						<Button
+							bgColor="$primary500"
+							borderRadius="$2xl"
+							onPress={handleLogin}
+						>
+							<ButtonText color="$white">Login</ButtonText>
+						</Button>
+						<Button
+							bgColor="$primary500"
+							borderRadius="$2xl"
+							onPress={handleSignup}
+						>
+							<ButtonText color="$white">Signup</ButtonText>
+						</Button>
+						{Platform.OS === 'web' && (
+							<Button
+								bgColor="$primary500"
+								borderRadius="$2xl"
+								onPress={onPressGoogle}
+							>
+								<ButtonText color="$white">
+									Login with Google
+								</ButtonText>
+							</Button>
+						)}
+					</VStack>
+				</VStack>
+			</FormControl>
 		</ScrollView>
 	);
 }
