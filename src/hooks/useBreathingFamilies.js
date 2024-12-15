2d4b21ed5406d648cf57c73aced2c2f47f8cf97a
Simone Guarnuccio <simoneguarnuccio@creationdose.com>
2024-12-15 18:50:25 +0100

    feat: integrate authentication check in useBreathingFamilies hook to manage data loading based on user state

--- src/hooks/useBreathingFamilies.js
+++ src/hooks/useBreathingFamilies.js
@@ -1,4 +1,5 @@
 import { getDatabase, onValue, ref } from 'firebase/database';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { useEffect, useState } from 'react';
 
 const useBreathingFamilies = () => {
@@ -9,23 +10,32 @@ const useBreathingFamilies = () => {
 	const refBreathingDB = ref(db, '/Respirazione/');
 
 	useEffect(() => {
-		let tmp = [];
-		let famiglia = {};
-		setFamilies([]);
+		const auth = getAuth();
+		const unsubscribe = onAuthStateChanged(auth, user => {
+			if (user) {
+				let tmp = [];
+				let famiglia = {};
+				setFamilies([]);
 
-		onValue(refBreathingDB, snapshot => {
-			setLoading(true);
-			snapshot.forEach(childSnap => {
-				famiglia = {
-					id: childSnap.key,
-					contenuto: childSnap.val(),
-				};
+				onValue(refBreathingDB, snapshot => {
+					setLoading(true);
+					snapshot.forEach(childSnap => {
+						famiglia = {
+							id: childSnap.key,
+							contenuto: childSnap.val(),
+						};
 
-				tmp.push(famiglia);
-			});
-			setFamilies(tmp);
-			setLoading(false);
+						tmp.push(famiglia);
+					});
+					setFamilies(tmp);
+					setLoading(false);
+				});
+			} else {
+				setLoading(false);
+			}
 		});
+
+		return () => unsubscribe();
 	}, []);
 
 	return { families, loading };
