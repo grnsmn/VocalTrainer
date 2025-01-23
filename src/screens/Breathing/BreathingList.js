17a6d105a3862ecd82bd0d66422e1361b66ab8b4
Simone Guarnuccio <simoneguarnuccio@creationdose.com>
2025-01-23 09:01:50 +0100

    feat: load sound effects in BreathingList component

--- src/screens/Breathing/BreathingList.js
+++ src/screens/Breathing/BreathingList.js
@@ -1,20 +1,40 @@
 import { useState, useEffect } from 'react';
 import { FlatList } from '@gluestack-ui/themed';
 import CardSelect from '../../components/CardSelect';
+import useStore from '../../store';
+import { Audio } from 'expo-av';
 
 const BreathingList = ({ route, navigation }) => {
 	const { Titoli, famiglia } = route.params;
 	const [listaTitoli, setListaTitoli] = useState(Object.values(Titoli));
 	const [scelta, setScelta] = useState('');
+	const { setSounds, sounds } = useStore();
+
 	//console.log(Object.values(Titoli));
 
 	useEffect(() => {
+		const loadSounds = async () => {
+			const click1 = await Audio.Sound.createAsync(
+				require('../../../assets/sounds/click1.mp3'),
+			);
+			const click2 = await Audio.Sound.createAsync(
+				require('../../../assets/sounds/click2.mp3'),
+			);
+
+			setSounds({
+				click1: click1.sound,
+				click2: click2.sound,
+			});
+		};
+
 		navigation.setOptions({
 			title: famiglia,
 			headerTitleStyle: {
 				fontSize: 24,
 			},
 		});
+
+		loadSounds();
 	}, []);
 
 	const loadEsercizio = item => {
