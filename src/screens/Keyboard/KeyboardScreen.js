fcd5a33ba08819fe1bbd13890a620df01ee44fe8
Simone Guarnuccio <simoneguarnuccio@creationdose.com>
2025-05-30 09:24:36 +0200

    fix(keyboard): adjust KeyboardScreen layout for better user experience
    - Changed justifyContent from 'center' to 'flex-end' in the KeyboardScreen styles.
    
    This adjustment improves the layout of the keyboard interface, enhancing usability.

--- src/screens/Keyboard/KeyboardScreen.js
+++ src/screens/Keyboard/KeyboardScreen.js
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#FFFFFF',
-		justifyContent: 'center',
+		justifyContent: 'flex-end',
 		alignItems: 'center',
 	},
 	keyboard: {
