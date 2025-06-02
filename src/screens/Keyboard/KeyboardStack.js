5acd4bc51d000cb1155893ec59cab32c7ca14e26
Simone Guarnuccio <simoneguarnuccio@creationdose.com>
2025-06-02 12:52:38 +0200

    refactor(KeyboardStack): remove unused header options
    - Removed the unused screenOptions object and its properties from KeyboardStack.js.
    - This change simplifies the code and improves readability by eliminating unnecessary configurations.
    
    These updates contribute to a cleaner and more maintainable codebase.

--- src/screens/Keyboard/KeyboardStack.js
+++ src/screens/Keyboard/KeyboardStack.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import KeyboardScreen from './KeyboardScreen';
-import HeaderRight from '../../components/HeaderRight';
 
 const KeyboardStack = createNativeStackNavigator();
-
-const screenOptions = {
-	headerTitleAlign: 'center',
-	headerStyle: { backgroundColor: '#c6e9ff' },
-	title: 'Vocal Trainer',
-};
 
 function KeyboardStackScreen() {
 	return (
