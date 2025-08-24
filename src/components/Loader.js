d603957043b543b796a15188b18ee5b8afd84fb2
Simone Guarnuccio <simoneguarnuccio@creationdose.com>
2025-08-24 23:40:26 +0200

    feat(Loader): integrate Lottie animation for loading state
    - Replaced the static spinner with a Lottie animation for a more engaging loading experience.
    - Imported the jump_notes.json animation and utilized LottieView to display it within the Loader component.
    
    These updates enhance the visual feedback during loading, contributing to a better user experience.

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,15 +1,26 @@
-import { VStack } from "@/components/ui/vstack";
-import { Spinner } from "@/components/ui/spinner";
-import { Text } from "@/components/ui/text";
+import { VStack } from '@/components/ui/vstack';
+import { Spinner } from '@/components/ui/spinner';
+import { Text } from '@/components/ui/text';
+import AnimatedJumpNotes from '../../assets/lotties/jump_notes.json';
 import React from 'react';
+import LottieView from 'lottie-react-native';
 
 const Loader = () => {
 	return (
-        <VStack space="sm" className="flex-1 justify-center items-center">
-            <Spinner size="small" />
-            <Text size="md">Please Wait</Text>
-        </VStack>
-    );
+		<VStack space="sm" className="flex-1 justify-center items-center">
+			<LottieView
+				webStyle={{ width: 100, height: 100 }}
+				source={AnimatedJumpNotes}
+				autoPlay
+				loop={true}
+				style={{
+					width: 50,
+					height: 50,
+				}}
+			/>
+			<Text size="md">Please Wait</Text>
+		</VStack>
+	);
 };
 
 export default Loader;
