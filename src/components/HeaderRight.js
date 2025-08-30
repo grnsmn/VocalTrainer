3c961f16b7ab51cd32ab39255ad2ce28536139f1
Simone Guarnuccio <simoneguarnuccio@creationdose.com>
2025-08-30 22:04:49 +0200

    fix(HeaderRight): refactor logout functionality for improved error handling
    - Updated the handleLogout function to use async/await for better error management.
    - Changed the onPress event to onPressOut to address issues with React Navigation in the new architecture.
    - Removed redundant import statements to streamline the code.
    
    These changes enhance the logout feature's reliability and user interaction.

--- src/components/HeaderRight.js
+++ src/components/HeaderRight.js
@@ -1,8 +1,6 @@
+import React from 'react';
 import { Text } from '@/components/ui/text';
 import { Icon } from '@/components/ui/icon';
-// HeaderRight.js
-import React from 'react';
-
 import { getAuth, signOut } from 'firebase/auth';
 import { useAsyncStorage } from '@react-native-async-storage/async-storage';
 
@@ -14,17 +12,20 @@ const HeaderRight = () => {
 	const { clearAuth } = useStore();
 	const { removeItem } = useAsyncStorage('authData');
 
-	const handleLogout = () => {
-		const auth = getAuth();
-		signOut(auth).then(() => {
-			removeItem();
+	const handleLogout = async () => {
+		try {
+			const auth = getAuth();
+			await signOut(auth);
+
+			// Clean up local state
+			await removeItem();
 			clearAuth();
-		});
+		} catch (error) {}
 	};
 
 	return (
 		<Pressable
-			onPress={handleLogout}
+			onPressOut={handleLogout} // workaround for issue with pressable onPress with react navigation in new architecture https://github.com/react-navigation/react-navigation/issues/12039
 			android_ripple={{ color: 'rgba(0,0,0,0.1)' }}
 			hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
 			className="items-center justify-center web:p-2"
