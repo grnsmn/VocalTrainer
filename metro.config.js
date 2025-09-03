88dd22e8359a27fc6fe851c833130fa08808c3a5
GScriptS <simoneguarnuccio@gmail.com>
2025-09-03 10:06:09 +0200

    fix(metro.config): clean up formatting and improve comment clarity

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,4 @@
-const {
-    withNativeWind: withNativeWind
-} = require("nativewind/metro");
+const { withNativeWind: withNativeWind } = require('nativewind/metro');
 
 const { getDefaultConfig } = require('expo/metro-config');
 const path = require('path');
@@ -17,12 +15,13 @@ config.resolver = {
 		),
 	},
 	unstable_conditionNames: [
-		  'browser',
-  			'require',
- 		 'react-native',
-	]
+		// workaround https://github.com/pmndrs/zustand/discussions/1967#discussioncomment-9578159
+		'browser',
+		'require',
+		'react-native',
+	],
 };
 
 module.exports = withNativeWind(config, {
-    input: "./global.css"
+	input: './global.css',
 });
