63ef0e55ec147ac630f9e41c72382d8cc2fe117a
GScriptS <simoneguarnuccio@gmail.com>
2025-09-13 09:36:36 +0200

    fix(minSdkProjectGradlePatch.js): update targetSdkVersion to 35 and clean up code formatting

--- plugins/minSdkProjectGradlePatch.js
+++ plugins/minSdkProjectGradlePatch.js
@@ -1,4 +1,3 @@
-
 const { withProjectBuildGradle } = require('@expo/config-plugins');
 const generateCode = require('@expo/config-plugins/build/utils/generateCode');
 
@@ -8,30 +7,32 @@ const minSdkPatchCode = `
     buildToolsVersion = findProperty('android.buildToolsVersion') ?: '35.0.0'
     minSdkVersion = Integer.parseInt(findProperty('android.minSdkVersion') ?: '24')
     compileSdkVersion = Integer.parseInt(findProperty('android.compileSdkVersion') ?: '35')
-    targetSdkVersion = Integer.parseInt(findProperty('android.targetSdkVersion') ?: '34')
+    targetSdkVersion = Integer.parseInt(findProperty('android.targetSdkVersion') ?: '35')
     kotlinVersion = findProperty('android.kotlinVersion') ?: '2.0.21'
   }
 `;
 
 module.exports = function withMinSdkProjectGradlePatch(config) {
-    return withProjectBuildGradle(config, async (config) => {
-        if (config.modResults.contents.includes(minSdkPatchCode)) {
-            return config;
-        }
+	return withProjectBuildGradle(config, async config => {
+		if (config.modResults.contents.includes(minSdkPatchCode)) {
+			return config;
+		}
 
-        const addCode = generateCode.mergeContents({
-            newSrc: minSdkPatchCode,
-            tag: 'minSdkPatchCode',
-            src: config.modResults.contents,
-            anchor: 'buildscript {',
-            comment: '//',
-            offset: 1,
-        });
+		const addCode = generateCode.mergeContents({
+			newSrc: minSdkPatchCode,
+			tag: 'minSdkPatchCode',
+			src: config.modResults.contents,
+			anchor: 'buildscript {',
+			comment: '//',
+			offset: 1,
+		});
 
-        config.modResults.contents = addCode.contents;
+		config.modResults.contents = addCode.contents;
 
-        console.log('[minSdkProjectGradlePatch] Gradle patch applied successfully!')
+		console.log(
+			'[minSdkProjectGradlePatch] Gradle patch applied successfully!',
+		);
 
-        return config;
-    });
+		return config;
+	});
 };
