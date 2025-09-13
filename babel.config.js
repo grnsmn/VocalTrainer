5275a95b8409cdc21b828f49c5f29e3871c45748
GScripts <simoneguarnuccio@gmail.com>
2025-09-13 17:57:11 +0200

    feat: update expo sdk 53 (#48)
    * chore: update dependencies and migrate from expo-av to expo-audio
    
    - Updated @expo/metro-runtime to version 5.0.4
    - Updated @react-native-async-storage/async-storage to version 2.1.2
    - Updated expo and related packages to latest versions
    - Updated lottie-react-native to version 7.2.2
    - Updated react, react-dom, and react-native to latest versions
    - Updated react-native-reanimated, react-native-safe-area-context, react-native-screens, react-native-svg, and react-native-web to latest versions
    - Updated react-native-webview to version 13.13.5
    - Changed import from expo-av to expo-audio in Esercizio.js and VocalizationsList.js
    
    * fix(metro.config): clean up formatting and improve comment clarity
    
    * fix: update react-native-svg to version 15.12.1 and add overrides for React version
    
    * fix: update expo-av to version 15.1.7 in package.json and package-lock.json
    
    * fix(app.json): change newArchEnabled to userInterfaceStyle for automatic UI
    
    * fix: update import from expo-audio to expo-av in Esercizio.js and VocalizationsList.js
    
    * fix(app.json): update Android build properties for Kotlin and SDK versions
    
    * fix(app.json): add gradlePluginVersion to Android build properties
    
    * fix: update Android build properties for Kotlin and SDK versions; add minSdkProjectGradlePatch plugin
    
    * fix(minSdkProjectGradlePatch.js): update targetSdkVersion to 35 and clean up code formatting
    
    * chore(minSdkProjectGradlePatch.js): comment out console log for Gradle patch application

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,8 @@ module.exports = function (api) {
     api.cache(true);
     return {
 		presets: [["babel-preset-expo", {
-            jsxImportSource: "nativewind"
+            jsxImportSource: "nativewind",
+            unstable_transformImportMeta: true,
         }], "nativewind/babel"],
 		plugins: [[
             'module:react-native-dotenv',
--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,4 @@
-const {
-    withNativeWind: withNativeWind
-} = require("nativewind/metro");
+const { withNativeWind: withNativeWind } = require('nativewind/metro');
 
 const { getDefaultConfig } = require('expo/metro-config');
 const path = require('path');
@@ -16,8 +14,14 @@ config.resolver = {
 			'node_modules/react-native-web-lottie',
 		),
 	},
+	unstable_conditionNames: [
+		// workaround https://github.com/pmndrs/zustand/discussions/1967#discussioncomment-9578159
+		'browser',
+		'require',
+		'react-native',
+	],
 };
 
 module.exports = withNativeWind(config, {
-    input: "./global.css"
+	input: './global.css',
 });
