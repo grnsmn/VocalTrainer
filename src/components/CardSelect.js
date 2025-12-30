0ef079c6de7f1c59f3ac71a05b3443218a14cd0b
GScriptS <simoneguarnuccio@gmail.com>
2025-12-30 15:11:16 +0100

    feat: Replace `Pressable` with `PressableScale` and adjust its props and styling for animated interactions.

--- src/components/CardSelect.js
+++ src/components/CardSelect.js
@@ -1,9 +1,9 @@
-import { Pressable } from "@/components/ui/pressable";
 import { Heading } from "@/components/ui/heading";
 import { HStack } from "@/components/ui/hstack";
 import { ChevronsRightIcon, Icon } from "@/components/ui/icon";
 import { Card } from "@/components/ui/card";
 import React, { memo } from 'react';
+import { PressableScale } from 'pressto';
 
 const CardSelect = ({ onPress, title }) => {
 	const handleOnPress = () => {
@@ -13,16 +13,16 @@ const CardSelect = ({ onPress, title }) => {
 	};
 
 	return (
-        <Pressable onPress={handleOnPress} className="m-2">
-            <Card size="lg" variant="elevated">
+		<PressableScale onPress={handleOnPress} android_ripple={null} animationType='spring' style={{ borderRadius: 12 }}>
+			<Card size="lg" variant="elevated" className="m-2">
 				<HStack className="justify-between items-center">
 					<Heading size="md">{title}</Heading>
 
 					<Icon as={ChevronsRightIcon} />
 				</HStack>
 			</Card>
-        </Pressable>
-    );
+		</PressableScale>
+	);
 };
 
 export default memo(CardSelect);
