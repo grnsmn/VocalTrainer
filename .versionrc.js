afc68f08791a011a5bb1d4e2aba89576a9a00ad3
Simone Guarnuccio <simoneguarnuccio@creationdose.com>
2026-01-10 20:28:41 +0100

    chore: translate all comments to English

--- .versionrc.js
+++ .versionrc.js
@@ -1,19 +1,19 @@
 module.exports = {
-  // File da cui leggere la versione corrente
+  // File to read current version from
   packageFiles: [{ filename: 'package.json', type: 'json' }],
   
-  // File da aggiornare con la nuova versione
+  // Files to update with new version
   bumpFiles: [
-    // package.json - versione npm standard
+    // package.json - standard npm version
     { filename: 'package.json', type: 'json' },
     
-    // app.json - expo.version (es. "2.1.0")
+    // app.json - expo.version (e.g. "2.1.0")
     { filename: 'app.json', updater: require.resolve('standard-version-expo') },
     
-    // app.json - android.versionCode (numero intero incrementale)
+    // app.json - android.versionCode (incremental integer)
     { filename: 'app.json', updater: require.resolve('standard-version-expo/android') },
     
-    // app.json - ios.buildNumber (stringa incrementale)
+    // app.json - ios.buildNumber (incremental string)
     { filename: 'app.json', updater: require.resolve('standard-version-expo/ios') }
   ]
 };
